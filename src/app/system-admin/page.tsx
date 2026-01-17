1f5eeef3be0ae10e39f254821d7ef46477595d57
DimonFilin <dimas.filimontsev@gmail.com>
2026-01-17 12:29:48 +0300

    feat(login): added auto logout for personalized pages

--- src/app/system-admin/page.tsx
+++ src/app/system-admin/page.tsx
@@ -1,10 +1,5 @@
-import { AppShell } from '@/widgets/layout/AppShell';
 import { RoleDashboard } from '@/widgets/role-dashboard/ui/RoleDashboard';
 
 export default function SystemAdminPage() {
-  return (
-    <AppShell>
-      <RoleDashboard expectedRole="SYSTEM_ADMIN" />
-    </AppShell>
-  );
+  return <RoleDashboard expectedRole="SYSTEM_ADMIN" />;
 }
